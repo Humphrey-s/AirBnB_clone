b2a7c8d722bb3540549066c783d2c53d2837e52c
greybeard <greybeard@gmail.com>
2024-06-04 16:11:55 +0300

    console

--- console.py
+++ console.py
@@ -4,7 +4,7 @@
 import cmd
 from datetime import datetime
 import models
-from models.amenity import Amenity
+#from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
@@ -13,7 +13,7 @@ from models.state import State
 from models.user import User
 import shlex  # for splitting the line along spaces except in double quotes
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+classes = {"BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
 
