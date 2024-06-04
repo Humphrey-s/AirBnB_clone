78889139c0d55fbb167eab14250a62bcd9842f51
greybeard <greybeard@gmail.com>
2024-06-04 20:02:11 +0300

    ll

--- console.py
+++ console.py
@@ -75,19 +75,17 @@ class HBNBCommand(cmd.Cmd):
         args = shlex.split(arg)
         if len(args) == 0:
             print("** class name missing **")
-            return False
-        if args[0] not in classes:
+        elif args[0] not in classes.keys():
             print("** class doesn't exist **")
-            return False
-
-        if len(args) > 1:
-            key = args[0] + "." + args[1]
-            if key in models.storage.all():
-                print(models.storage.all()[key])
-            else:
-                print("** no instance found **")
         else:
-            print("** instance id missing **")
+            if len(args) > 1:
+                key = args[0] + "." + args[1]
+                if key in models.storage.all():
+                    print(models.storage.all()[key])
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
 
 
     def do_destroy(self, arg):
